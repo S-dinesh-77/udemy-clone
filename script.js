16875144695dcfce623f450e24f7e38de9010a99
Dinesh <162544458+S-dinesh-77@users.noreply.github.com>
2024-09-30 14:23:09 +0530

    Updated project structure

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('menuIcon').addEventListener('click', function() {
+    const courses = document.getElementById('courses');
+    courses.classList.toggle('show'); // Toggle the visibility of courses
+});
